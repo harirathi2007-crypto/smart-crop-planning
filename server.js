71ada348df62ac890189d8c04a4aab6bcf98d2b0
Hariharan <harirathi2007@gmail.com>
2026-09-22 14:24:50 +0530

    Create Express backend server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Smart Crop Planning System API is running"
+    });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
